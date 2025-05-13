83e765e8b05f5ba472d0a415b8e83d036708f8d7
Miguel26-cat <migtorlop@alu.edu.gva.es>
2025-05-13 21:45:33 +0200

    Modification of customers.fxml and back button

--- src/main/java/finalproject/glamourfx/controllers/DatesInterface.java
+++ src/main/java/finalproject/glamourfx/controllers/DatesInterface.java
@@ -1,4 +1,40 @@
 package finalproject.glamourfx.controllers;
 
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.stage.Stage;
+
+import java.io.IOException;
+
 public class DatesInterface {
+
+
+
+
+    @FXML
+    private void back(ActionEvent actionEvent) {
+        try
+        {
+            FXMLLoader loader = new FXMLLoader(getClass().getResource("/finalproject/glamourfx/admin.fxml"));
+            Parent root = loader.load();
+            Scene scene = new Scene(root);
+
+            Stage stage = (Stage) ((Node) actionEvent.getSource()).getScene().getWindow();
+            stage.setTitle("GlamourFX");
+            stage.setScene(scene);
+
+            stage.setFullScreen(true);
+            stage.setFullScreenExitHint("");
+
+            stage.show();
+        }
+        catch (IOException e)
+        {
+            e.printStackTrace();
+        }
+    }
 }
