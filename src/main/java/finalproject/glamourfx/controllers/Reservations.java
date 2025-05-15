efc29973c8448197b00dbb987526aa1b3fa09b23
Nehuén Rojas Marsicano <nehrojmar@alu.edu.gva.es>
2025-05-15 23:28:57 +0200

    Add the reservations class with its respective buttons and dropdowns. Two new files, reservations.txt and services.txt

---
+++ src/main/java/finalproject/glamourfx/controllers/Reservations.java
@@ -0,0 +1,155 @@
+package finalproject.glamourfx.controllers;
+
+import finalproject.glamourfx.data.Customer;
+import javafx.animation.PauseTransition;
+import javafx.animation.RotateTransition;
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.FXMLLoader;
+import javafx.fxml.Initializable;
+import javafx.scene.Node;
+import javafx.scene.Parent;
+import javafx.scene.Scene;
+import javafx.scene.control.*;
+import javafx.scene.transform.Rotate;
+import javafx.stage.Stage;
+import javafx.util.Duration;
+import java.time.LocalDate;
+import java.io.*;
+import java.net.URL;
+import java.util.ArrayList;
+import java.util.List;
+import java.util.ResourceBundle;
+
+public class Reservations implements Initializable
+{
+    private static Reservations instance;
+
+    @FXML
+    private Button cancelButton;
+    @FXML
+    private Button confirmButton;
+    @FXML
+    private Button backButton;
+    @FXML
+    private ChoiceBox<String> selectHairdresser;
+    @FXML
+    private ChoiceBox<String> selectService;
+    @FXML
+    private DatePicker datePicker;
+    @FXML
+    private Label errorLabel;
+    @FXML
+    private Label cancelLabel;
+    @FXML
+    private Label confirmLabel;
+
+    private void loadHairdressers()
+    {
+        try (BufferedReader reader = new BufferedReader(new FileReader("hairdressers.txt")))
+        {
+            String line;
+            while ((line = reader.readLine()) != null)
+            {
+                String[] parts = line.split(";");
+                if (parts.length >= 1)
+                {
+                    String name = parts[0].trim();
+                    selectHairdresser.getItems().add(name);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+    private void loadServices()
+    {
+        try (BufferedReader reader = new BufferedReader(new FileReader("services.txt")))
+        {
+            String line;
+            while ((line = reader.readLine()) != null)
+            {
+                String[] parts = line.split(";");
+                if (parts.length >= 1)
+                {
+                    String name = parts[0].trim();
+                    selectService.getItems().add(name);
+                }
+            }
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void cancelReservation(ActionEvent event)
+    {
+        selectHairdresser.getSelectionModel().clearSelection();
+        selectService.getSelectionModel().clearSelection();
+        datePicker.setValue(null);
+        setCancelStars("Reservation cancelled");
+    }
+
+    @FXML
+    private void confirmReservation(ActionEvent event)
+    {
+        String hairdresser = selectHairdresser.getValue();
+        String service = selectService.getValue();
+        LocalDate datePicker2 = datePicker.getValue();
+
+        if (hairdresser == null || service == null || datePicker2 == null) {
+            setErrorStars("Por favor, completa todos los campos.");
+            return;
+        }
+        String reservationData = hairdresser + ";" + service + ";" + datePicker2.toString();
+
+        try (BufferedWriter writer = new BufferedWriter(new FileWriter("reservations.txt", true))) {
+            writer.write(reservationData);
+            writer.newLine();
+            setConfirmStars("Reserva guardada: " + reservationData);
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+    }
+
+    @FXML
+    private void backToMainMenu(ActionEvent event) throws IOException
+    {
+        Parent root = FXMLLoader.load(getClass().getResource("/finalproject/glamourfx/customer.fxml"));
+        Scene scene = new Scene(root);
+        Stage stage = (Stage) ((Node) event.getSource()).getScene().getWindow();
+        stage.setScene(scene);
+        stage.show();
+    }
+
+
+    //Mensajes de confirmación, error y cancelación
+    public void setErrorStars(String nombre)
+    {
+        errorLabel.setText(nombre);
+        PauseTransition delay = new PauseTransition(Duration.seconds(2));
+        delay.setOnFinished(e -> errorLabel.setText(""));
+        delay.play();
+    }
+    public void setConfirmStars(String nombre)
+    {
+       confirmLabel.setText(nombre);
+        PauseTransition delay = new PauseTransition(Duration.seconds(2));
+        delay.setOnFinished(e -> confirmLabel.setText(""));
+        delay.play();
+    }
+    public void setCancelStars(String nombre)
+    {
+        cancelLabel.setText(nombre);
+        PauseTransition delay = new PauseTransition(Duration.seconds(2));
+        delay.setOnFinished(e -> cancelLabel.setText(""));
+        delay.play();
+    }
+
+    @Override
+    public void initialize(URL url, ResourceBundle resourceBundle)
+    {
+        loadHairdressers();
+        loadServices();
+    }
+}
